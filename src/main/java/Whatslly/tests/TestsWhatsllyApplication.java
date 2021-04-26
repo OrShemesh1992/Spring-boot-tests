dfc3fc63da106142a78b7822f6c2e3dfdb1b7e95
orshemesh <chikos2011@gmail.com>
2021-04-26 14:22:49 +0300

    first commit

---
+++ src/main/java/Whatslly/tests/TestsWhatsllyApplication.java
@@ -0,0 +1,13 @@
+package Whatslly.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsWhatsllyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsWhatsllyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Whatslly/tests/TestsWhatsllyApplicationTests.java
@@ -0,0 +1,13 @@
+package Whatslly.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsWhatsllyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
