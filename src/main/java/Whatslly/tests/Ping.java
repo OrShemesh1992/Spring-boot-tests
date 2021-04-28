5ded61c9035cbac42790afab64a7f7612d871996
orshemesh <chikos2011@gmail.com>
2021-04-28 22:45:54 +0300

    finish

--- src/main/java/Whatslly/tests/Ping.java
+++ src/main/java/Whatslly/tests/Ping.java
@@ -1,49 +1,117 @@
 package Whatslly.tests;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
+import java.io.InputStreamReader;
+import java.io.Writer;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.Connection.Method;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 import com.google.gson.Gson;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+/**
+ * class Ping represents the file json from the server
+ * @author orshemesh
+ *
+ */
 public class Ping {
-	private String success;
+	//variables
+	private Boolean success;
 	private String sfVersion;
 	private String host;
 	private String version;
 	private String saEndpoint;
 	private Boolean newPackage;
 	
-	public Ping(String html) {
-		// connect to html example html = https://api-us1.whatslly.com/test/ping.json
-        Connection conn = Jsoup.connect(html).ignoreContentType(true).method(Method.GET);
-        Connection.Response response;
-    	try {
-			response = conn.execute();
-			Document parsed = response.parse();
-	        Element form = parsed.select("body").first();
-	        //parse json string to onject ping by gson of google
-	        Gson g = new Gson(); 
-	        Ping temp = g.fromJson(form.text(), Ping.class) ;
+	//constructor
+	public Ping() {
+		super();
+	}
+	
+	//copy constructor
+	public void init(Ping other) {
+		this.success=other.getSuccess();
+		this.sfVersion=other.getSfVersion();
+		this.host=other.getHost();
+		this.version=other.getVersion();
+		this.saEndpoint=other.getSaEndpoint();
+		this.newPackage=other.getNewPackage();
+	}
+	
+	/**
+	 * The function gets a file name ans then 
+	 * reads from file and convert to object "Ping" 
+	 * I used "Gson" to load object
+	 * @param file_name
+	 * @return object "Ping" 
+	 * @throws FileNotFoundException
+	 */
+	public Ping loadFromJson(String file_name) throws FileNotFoundException {
+		//read from file to JsonObject
+		FileInputStream input = new FileInputStream(file_name);
+		JsonReader read = new JsonReader(new InputStreamReader(input));
+		JsonObject element = JsonParser.parseReader(read).getAsJsonObject(); 
+		
+		//convert JsonObject to object "Ping" 
+		Gson g = new Gson(); 
+	    Ping temp = g.fromJson(element, Ping.class);
+
+	    return temp;
+	}
+	
+	/**
+	 * The function gets a file name and then write to file 
+	 * I used "Gson" to write object
+	 * @param file_name
+	 */
+	public void WriteToJson(String file_name) {
+		try {
 			
-	        this.success = temp.getSuccess();
-	        this.sfVersion = temp.getSfVersion();
-	        this.host = temp.getHost();
-	        this.version = temp.getVersion();
-	        this.saEndpoint = temp.getSaEndpoint();
-	        this.newPackage = temp.getNewPackage();
+			Writer writer = new FileWriter(file_name);
+		    Gson gson = new Gson();
+		    gson.toJson(this , writer);
+		    writer.flush(); //flush data to file   <---
+		    writer.close(); //close write          <---
+		    
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 *  The function gets a host name Connecting and then from
+	 *  html file get json string and initialize this object
+	 * @param html
+	 */
+	public void loadFromHtml(String html) {
+		try {
+			// connect to html example html = https://api-us1.whatslly.com/test/ping.json
+			//jsoup libary
+	        Connection conn = Jsoup.connect(html).ignoreContentType(true).method(Method.GET);
+	        Connection.Response response= conn.execute();
+			
+	        //get element from respone
+	        Document parsed = response.parse();
+		    Element form = parsed.select("body").first();
+		    
+		    //parse json string to object ping by gson of google
+		    Gson g = new Gson(); 
+		    Ping temp = g.fromJson(form.text(), Ping.class);
+		    
+		    //initialization
+		    init(temp);
+		    
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public String getSuccess() {
+	public Boolean getSuccess() {
 		return success;
 	}
 	public String getSfVersion() {
@@ -66,11 +134,6 @@ public class Ping {
 	public String toString() {
 		return "Ping [success=" + success + ", sfVersion=" + sfVersion + ", host=" + host + ", version=" + version
 				+ ", saEndpoint=" + saEndpoint + ", newPackage=" + newPackage + "]";
-	}
-	
-	public static void main(String[] args) {
-		Ping a = new Ping("https://api-us1.whatslly.com/test/ping.json");
-		System.out.println(a);
 	}
 
 }
--- src/test/java/Whatslly/tests/TestsWhatsllyApplicationTests.java
+++ src/test/java/Whatslly/tests/TestsWhatsllyApplicationTests.java
@@ -1,13 +1,112 @@
 package Whatslly.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ *		      Tests for all servers
+ 	
+ 		https://api-us1.whatslly.com/test/ping.json
+		https://api-br1.whatslly.com/test/ping.json
+		https://api-eu1.whatslly.com/test/ping.json
+		https://api-st1.whatslly.com/test/ping.json
+		
+ * @author Or Shemesh
+ *
+ */
+
 @SpringBootTest
 class TestsWhatsllyApplicationTests {
+	
+	//variables
+	private static Ping p=new Ping();
+	private static String host;
+	
+	@BeforeAll
+	static void setUp() throws Exception {
+		//Request user to enter host Example: https://api-us1.whatslly.com/test/ping.json
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter a html test: ");
+		host= sc.nextLine(); 
+		
+		// load from host json to object "ping"
+		p.loadFromHtml(host);
 
-	@Test
-	void contextLoads() {
+
+		// Get the file
+		File f = new File("Output.json");
+
+		// Check if the specified file
+		// Exists or not
+		if (f.exists()) {
+			System.out.println("Exists");
+		}
+		else {
+			p.WriteToJson("Output.json");
+		}
+
 	}
 
+	@Test
+	void Status() {
+		//check if status true
+		assertEquals(p.getSuccess(),true);
+	}	
+	@Test
+	void host() {
+		// split string host by /
+		//Example: [https:],[api-us1.whatslly.com],[test],[ping.json]
+		String[] slice1 = host.split("/"); //returns an array with the 2 parts
+		String[] slice2 = p.getHost().split("/");
+		String host1="";
+		String host2="";
+		
+		//Takes only the part of the host
+		//Example: [api-us1.whatslly.com]
+		for (int i = 0; i < slice1.length; i++) {
+			if(slice1[i].contains("api")) {
+				host1=slice1[i]; 
+			}
+		}
+		for (int i = 0; i < slice2.length; i++) {
+			if(slice2[i].contains("api")) {
+				host2=slice2[i]; 
+			}
+		}
+		//check if host is correct
+		assertEquals(host1,host2);
+	}	
+
+	@Test
+	void version() {
+		Ping old_version;
+		try {
+			//get old version from older file json 
+			old_version = p.loadFromJson("Output.json");
+
+			//split version by "." [5],[5],[53]
+			String[] old_V= old_version.getVersion().split("\\.");
+			String[] new_V= p.getVersion().split("\\.");
+
+			//Checks if the new version is larger than the old version
+			for (int i = 0; i < new_V.length; i++) {
+				assertTrue(Integer.parseInt(old_V[i])<=Integer.parseInt(new_V[i]));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+	}
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		//if all tests pass load new json file 
+		p.WriteToJson("Output.json");
+	}
 }
